e60ebeb28a5c87fb25d418d664c842f58f7eff65
JohanMillberg <johanmillberg@me.com>
2022-11-08 13:27:48 +0100

    reformatted and implemented noise in the created images

--- fracture_generator.py
+++ fracture_generator.py
@@ -16,7 +16,9 @@ class FractureGenerator:
                  buffer_size: int,
                  max_length: float = 160.0,
                  std_dev_length: float = 30.0,
-                 std_dev_angle: float = 30,
+                 std_dev_angle: float = 30.0,
+                 mean_noise: float = 1.0,
+                 std_dev_noise: float = 0.2,
                  max_iterations: int = 15):
 
         self.image_height = image_height
@@ -25,11 +27,14 @@ class FractureGenerator:
         self.fracture_width = fracture_width
         self.buffer_size = buffer_size
         self.max_length = max_length
+
         self.length_distribution = truncnorm(0, max_length, loc=max_length, scale=std_dev_length)
         self.angle_distribution = norm(loc=0, scale=std_dev_angle)
-        self.max_iterations = max_iterations
         self.x_distribution = uniform(loc=0, scale=self.image_width)
         self.y_distribution = uniform(loc=0, scale=self.image_height)
+        self.noise_distribution = truncnorm(0, np.Inf, loc=mean_noise, scale=std_dev_noise)
+
+        self.max_iterations = max_iterations
 
     def generate_fractures(self, image: tf.Tensor):
         fracture_image = tf.zeros_like(image).numpy()
@@ -37,21 +42,25 @@ class FractureGenerator:
             fracture_is_valid = False
             n_iterations = 0
 
-            while not fracture_is_valid and n_iterations < self.max_iterations: 
+            while (not fracture_is_valid) and (n_iterations < self.max_iterations): 
                 fracture_length = self.length_distribution.rvs().astype(int)
                 fracture_angle = self.angle_distribution.rvs()
-                xs, ys = self._sample_coordinates()
-
                 pixels_to_fracture = []
 
+                # Sample a valid starting position for the fracture
+                current_sample_iteration = 0
+                xs, ys = self._sample_coordinates(current_sample_iteration)
+
                 while self._is_invalid_pixel(fracture_image, xs, ys):
-                    xs, ys = self._sample_coordinates()                
+                    current_sample_iteration += 1
+                    xs, ys = self._sample_coordinates(current_sample_iteration)                
 
                 pixels_to_fracture.append((xs, ys))
 
                 x_exact = xs
                 y_exact = ys
 
+                # Add the rest of the pixels to the fracture
                 fractured_pixels = 1
                 while fractured_pixels < fracture_length:
                     x_exact = x_exact + np.cos(fracture_angle)
@@ -68,13 +77,19 @@ class FractureGenerator:
                         pixels_to_fracture.append((x_index, y_index))
                         fractured_pixels += 1
 
+                # Create the fracture
                 fracture_is_valid = True
                 self._create_buffer(fracture_image, pixels_to_fracture)
                 for x, y in pixels_to_fracture:
                     self._fracture_pixel(fracture_image, x, y)
 
-        fracture_image[fracture_image == -1] = 0
+            if not fracture_is_valid:
+                raise RuntimeError("Unable to fit fracture in image")
+
+        # Produce the resulting image
+        fracture_image[fracture_image == -1] = 0 # Remove the buffer
         fracture_image = self._blur_fracture_edges(fracture_image)
+        fracture_image = self._add_noise(fracture_image, 1, 0.1)
         fracture_image = tf.convert_to_tensor(fracture_image)
         resulting_image = tf.math.add(image, fracture_image)
 
@@ -108,7 +123,10 @@ class FractureGenerator:
         return x < 0 or x >= self.image_width or \
                y < 0 or y >= self.image_height
 
-    def _sample_coordinates(self):
+    def _sample_coordinates(self, current_sample_iteration: int):
+        if current_sample_iteration > self.max_iterations:
+            raise RuntimeError("Unable to fit fracture in image")
+
         xs = self.x_distribution.rvs().astype(int)
         ys = self.y_distribution.rvs().astype(int)
         return xs, ys
@@ -117,19 +135,29 @@ class FractureGenerator:
         if self._out_of_bounds(x, y):
             return True
         
-        if self._collides_with_fracture(image, x, y):
+        elif self._collides_with_fracture(image, x, y):
             return True
         
-        if self._pixel_in_buffer(image, x, y):
+        elif self._pixel_in_buffer(image, x, y):
             return True
 
-        return False
+        else:
+            return False
 
     def _collides_with_fracture(self, image, x, y):
         return image[x, y] == 2.0
 
     def _pixel_in_buffer(self, image, x, y):
         return image[x, y] == -1
+
+    def _add_noise(self, image: np.array, mean_noise: int, std_dev_noise: int):
+        rvs_vec = np.vectorize(self._noise_helper,
+                               excluded=['distribution'],
+                               otypes=['float32'])
+        return rvs_vec(p=image, distribution=self.noise_distribution)
+
+    def _noise_helper(self, p, distribution):
+        return p + distribution.rvs()
 
     def plot_image(self, image):
         plt.gray()
@@ -145,13 +173,21 @@ def normalize_image(image: tf.Tensor):
 
 
 def main():
+    # Specify parameters
     image_height = 150
     image_width = 300
     n_fractures = 10
     fracture_width = 2
-    buffer_size = 25
+    buffer_size = 20 # space between fractures
+    mean_noise = 1.0
+    std_dev_noise = 0.2
     max_length = 50
     std_dev_length = 10
+    std_dev_angle = 30.0
+    mean_noise = 1.0
+    std_dev_noise = 0.2
+    max_iterations = 15
+    n_images_to_generate = 1
 
     generator = FractureGenerator(image_height,
                                   image_width,
@@ -159,18 +195,26 @@ def main():
                                   fracture_width,
                                   buffer_size,
                                   max_length,
-                                  std_dev_length)
-    for i in range(5):
-        result = generator.generate_fractures(tf.zeros(
-                                            (image_height, image_width))
-                                            ).numpy()
+                                  std_dev_length,
+                                  std_dev_angle,
+                                  mean_noise,
+                                  std_dev_noise,
+                                  max_iterations)
 
+    for i in range(n_images_to_generate):
+        result = generator.generate_fractures(tf.zeros(
+                                                (image_height, image_width))
+                                                 ).numpy()
+    
         result = normalize_image(result)
         result = tf.expand_dims(result, 2)
+
+        # Save the images
         # tf.keras.utils.save_img(
         #    f"./images/fractured/im{i}.jpg",
         #    result
         # )
+
     generator.plot_image(result)
 
 
