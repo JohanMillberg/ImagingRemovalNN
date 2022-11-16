2e5da53488b0dc7d9f97e5747ca7c16fbc4d3cd8
moa-elisabeth <moaet25@gmail.com>
2022-11-16 15:13:56 +0100

    updated cholesky

--- cholesky.py
+++ cholesky.py
@@ -10,11 +10,10 @@ from scipy.linalg import sqrtm
 def ind(Ns, j):
     """ Help function for the mblockchol-function 
         Returns: a list of the "active indexes" of size Ns
-
         Corresponding to "ind = @(j) in (1:Ns) + Ns*(j-1)" in Matlab code
         Added a -1 since k in range(1, Nt) and we want to have starting index 0
     """
-    ind_t = np.linspace(1, Ns, Ns) + Ns*(j-1) - 1
+    ind_t = np.linspace(0, Ns, Ns) + Ns*(j-1)
     ind_list = [int(x) for x in ind_t]
     return ind_list
 
@@ -32,25 +31,34 @@ def mblockchol(M, Ns, Nt):
         msum = np.zeros([Ns, Ns])
         for j in range(1, k):
             msum = np.add(msum, \
-                np.matmul(L[ind(Ns, k)[0]:ind(Ns, k)[-1]+1, ind(Ns, j)[0]:ind(Ns, j)[-1]+1],\
-                          (L[ind(Ns, k)[0]:ind(Ns, k)[-1]+1, ind(Ns, j)[0]:ind(Ns, j)[-1]+1]).transpose()))
+                np.matmul(L[ind(Ns, k)[0]:ind(Ns, k)[-1], ind(Ns, j)[0]:ind(Ns, j)[-1]],\
+                          (L[ind(Ns, k)[0]:ind(Ns, k)[-1], ind(Ns, j)[0]:ind(Ns, j)[-1]]).transpose()))
 
         # Update L-matrix
-        L[ind(Ns, k)[0]:ind(Ns, k)[-1]+1,ind(Ns, k)[0]:ind(Ns, k)[-1]+1] = \
-             sqrtm(np.subtract(M[ind(Ns, k)[0]:ind(Ns, k)[-1]+1, ind(Ns, k)[0]:ind(Ns, k)[-1]+1],msum))
+        L[ind(Ns, k)[0]:ind(Ns, k)[-1],ind(Ns, k)[0]:ind(Ns, k)[-1]] = \
+             sqrtm(np.subtract(M[ind(Ns, k)[0]:ind(Ns, k)[-1], ind(Ns, k)[0]:ind(Ns, k)[-1]],msum))
 
-        for i in range(k+1, Nt+1):
+        for i in range(k, Nt+1):
             msum = np.zeros([Ns, Ns])
             for j in range(1, k):
                 msum = np.add(msum, \
-                    np.matmul(L[ind(Ns, i)[0]:ind(Ns, i)[-1]+1, ind(Ns, j)[0]:ind(Ns, j)[-1]+1],\
-                              L[ind(Ns, k)[0]:ind(Ns, k)[-1]+1, ind(Ns, j)[0]:ind(Ns, j)[-1]+1].transpose()))
+                    np.matmul(L[ind(Ns, i)[0]:ind(Ns, i)[-1], ind(Ns, j)[0]:ind(Ns, j)[-1]],\
+                              L[ind(Ns, k)[0]:ind(Ns, k)[-1], ind(Ns, j)[0]:ind(Ns, j)[-1]].transpose()))
 
             # Update L-matrix for the "inner"-matrices
-            M_new = np.subtract(M[ind(Ns, i)[0]:ind(Ns, i)[-1]+1, ind(Ns, k)[0]:ind(Ns, k)[-1]+1], msum)
-            norm_frac = L[ind(Ns, k)[0]:ind(Ns, k)[-1]+1, ind(Ns, k)[0]:ind(Ns, k)[-1]+1]
+            M_new = np.subtract(M[ind(Ns, i)[0]:ind(Ns, i)[-1], ind(Ns, k)[0]:ind(Ns, k)[-1]], msum)
+            norm_frac = L[ind(Ns, k)[0]:ind(Ns, k)[-1], ind(Ns, k)[0]:ind(Ns, k)[-1]]
 
-            L[ind(Ns, i)[0]:ind(Ns, i)[-1]+1, ind(Ns, k)[0]:ind(Ns, k)[-1]+1] =\
+            # print("\n\n")
+            # print(f"i = {i}")
+            # print(f"k = {k}")
+            # print("\nCholesky: norm_frac")
+            # print(norm_frac)
+
+            # print("\nCholesky: np.linalg.inv(norm_frac)")
+            # print(np.linalg.inv(norm_frac))
+            
+            L[ind(Ns, i)[0]:ind(Ns, i)[-1], ind(Ns, k)[0]:ind(Ns, k)[-1]] =\
                  np.matmul(M_new, np.linalg.inv(norm_frac))
 
     # Convert to final resulting matrix
