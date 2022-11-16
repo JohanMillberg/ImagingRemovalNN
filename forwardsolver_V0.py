a0910f0c461057b7a5c610efeb54603eaae34102
lovisaekeroot <loviisa_ekerot@hotmail.com>
2022-11-16 16:04:13 +0100

    adding TODO's for thursday

--- forwardsolver_V0.py
+++ forwardsolver_V0.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import sparse
 from cholesky import mblockchol
-import pandas as pd
 
 
 class ForwardSolver:
@@ -14,19 +13,27 @@ class ForwardSolver:
                 delta_x: float = 0.0063,
                 tau: float = 3.0303*10**(-5),
                 N_t: int = 70,
-                Bsrc_file: str = "Bsrc_T.txt"):
+                Bsrc_file: str = "Bsrc_T.txt",
+                N_x_im: int = 350,
+                N_y_im: int = 175,
+                O_x: int = 81,
+                O_y: int = 25):
 
         self.N = N_x
         self.N_s = N_s
         self.N_x = N_x
         self.N_y = N_y
         self.delta_x = delta_x
+        self.N_x_im = N_x_im
+        self.N_y_im = N_y_im
+        self.O_x = O_x
+        self.O_y = O_y
 
         self.tau = tau
         self.N_t = N_t
         self.delta_t = tau/20
 
-        self.wavelength = np.ones(self.N**2)
+        self.wavelength = 1000*np.ones(self.N**2).astype('float64')
         self.Bsrc_file = Bsrc_file 
 
     def import_sources(self):
@@ -39,15 +46,19 @@ class ForwardSolver:
     def init_simulation(self):
         # Calculate operators
         I_k = sparse.identity(self.N)
-        D_k = sparse.diags([1,-2,1],[-1,0,1], shape=(self.N,self.N))
-        L = (1/self.delta_x**2)*(sparse.kron(D_k, I_k) + sparse.kron(I_k, D_k)) 
-        C = 1000*sparse.diags(self.wavelength) 
+        D_k = (1/self.delta_x**2)*sparse.diags([1,-2,1],[-1,0,1], shape=(self.N,self.N))
+        D_k = sparse.csr_matrix(D_k)
+        D_k[0,0] = -1
 
-        A = - C @ L @ C
+        L = (sparse.kron(D_k, I_k) + sparse.kron(I_k, D_k))
+        C = (sparse.diags(self.wavelength))
+
+        A = (- C @ L @ C)
 
         u = np.zeros((3, self.N_x * self.N_y, self.N_s)) # Stores past, current and future instances
 
         b = self.import_sources()
+
         u[1] = b
         u[0] = (-0.5* self.delta_t**2 * A) @ b + b
 
@@ -62,28 +73,20 @@ class ForwardSolver:
         return ind_list
 
     def forward_solver(self):
-
         # Discretize time
         T = self.N_t * self.delta_t
+        nts = T/self.tau
         time = np.linspace(0, T, num=2*self.N_t)
         u, A, D, b = self.init_simulation()
 
-        # counter for data D
-        c = 0
-        for i in range(1,len(time)-1):
+        for i in range(1,len(time)):
             u[2] = u[1] 
             u[1] = u[0] 
             u[0] = (-self.delta_t**2 * A) @ u[1] - u[2] + 2*u[1]
 
-            # Update data
-            D[i] = np.transpose(b) @ u[1]
-
-            # For displaying images
-            """if i % 5 == 0:
-                plt.gray()
-                plt.title('FD solution at t = %f' %time[i])
-                plt.imshow(D[i])
-                plt.show()"""
+            if (i % nts) == 0:
+                D[i] = np.transpose(b) @ u[1]
+                D[i] = 0.5*(D[i].T + D[i])
 
         return D
 
@@ -99,27 +102,41 @@ class ForwardSolver:
                 M[ind_i[0]:ind_i[-1],ind_j[0]:ind_j[-1]] = 0.5 * (D[abs(i-j)] + D[abs(i+j)])
 
         R = mblockchol(M, self.N_s, self.N_t)
-
-        print("\n WE HAVE CALCULATED R !!!!!!!")
         print(R)
+    
+    ### New
+    def background_snapshots(self):
+        """
+        Function to calculate the orthogonalized background snapshots V_0
+        - size of V_0 = (N_x_im*N_y_im, N_s*N_t)
 
-        print("\nSee if R is calculated correctly")
-        temp_R_mul = np.multiply(R.T, R)
-        # if temp_R_mul.all() == M.all():
-        if (temp_R_mul == M).all():
-            print("R is calculated correctly")
-        else:
-            print("R.T * R is not equal to M :( ")
+        Calculate V_0 = U_0_im @ np.linalg.inv(R_0)
+        Calculate a new M_0 with new dimensions --> Calculate new R_0 with Cholesky
+        Use this R_0 to calculate V_0
+
+        DOUBLE CHECK DIMENSIONS OF O_x, O_y, N_x_im, N_y_im
+        """
+        u, A, D, b = self.init_simulation()
+        # u_0 = u[:, self.O_x:(self.O_x+self.N_x_im), self.O_y:(self.O_y+self.N_y_im)]
         
+        # Look at code in main to get the constant background velocity of 1000 over all vectors of small u
+        U_0 = 1000*sparse.diags(self.wavelength)
+    
+        print(np.shape(U_0))
 
-        # mat_R = np.matrix(R)
-        # df = pd.DataFrame(data=mat_R.astype(float))
-        # df.to_csv('R_matrix_test_2.csv', sep=' ', header=False, float_format='%.2f', index=False)
+
+    def imaging_func(self, V_0, R):
+        """
+        Imaging function at a point.
+        Find good way to use R and V_0! Then only np.linalg.norm()**2 for each i in 1, .., N_x_im*N_y_im
+        """
+        I = np.array([1, self.N_y_im, self.N_x_im])
+        for i in range():
+            I[i] = np.linalg.norm(V_0[i], np.linalg.inv(R))**2
 
 def main():
     solver = ForwardSolver()
-    solver.mass_matrix()
-    
+    solver.background_snapshots()
     
 if __name__ == "__main__":
     main()
