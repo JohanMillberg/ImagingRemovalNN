18e0e973f6027b251c7356ae3b8e9839618026c1
JohanMillberg <johanmillberg@me.com>
2022-11-30 20:03:31 +0100

    added script for plotting. Put images in images/labels or images/data

---
+++ plot_image.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, getopt
+
+filepath_label = f"labels/{sys.argv[1]}"
+filepath_data = f"data/{sys.argv[1]}"
+
+label = np.load(filepath_label)
+data = np.load(filepath_data)
+
+y_range = range(81, 81 + 350)
+x_range = range(25, 25 + 175)
+indices = [y*512 + x for y in y_range for x in x_range]
+label = label[indices].reshape((350, 175))
+    
+data = data.reshape((350, 175))
+
+data = (data - np.min(data)) / np.ptp(data)
+label = (label - np.min(label)) / np.ptp(label)
+
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(label.squeeze(), cmap="gray")
+ax[1].imshow(data.squeeze(), cmap="gray")
+plt.show()
