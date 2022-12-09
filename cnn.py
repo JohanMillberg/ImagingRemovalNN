0d9a9ebfd1de0637e058f8b9fa2c724264af5478
JohanMillberg <johanmillberg@me.com>
2022-12-09 18:37:14 +0100

    added new model

--- cnn.py
+++ cnn.py
@@ -15,43 +15,24 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 # tf.config.run_functions_eagerly(True)
 
-# model
-class ArtifactRemover(keras.Model):
-    def __init__(self):
-        super().__init__()
+def convolutional_autoencoder():
+    inputs = layers.Input(shape=(344, 168, 1))
 
-        self.encoder = keras.models.Sequential([
-            layers.Input(shape=(150, 300, 1)),
-            layers.Conv2D(16, (15, 15), activation='relu', padding='same', strides=3),
-            layers.Conv2D(8, (6, 6), activation='relu', padding='same', strides=2)
-        ])
+    p1 = layers.Conv2D(8, (2, 2), activation='relu', padding='same', strides=2)(inputs)
+    p2 = layers.Conv2D(16, (2, 2), activation='relu', padding='same', strides=2)(p1)
+    p3 = layers.Conv2D(32, (2, 2), activation='relu', padding='same', strides=2)(p2)
 
-        self.decoder = keras.models.Sequential([
-            layers.Conv2DTranspose(8, kernel_size=6, strides=2, activation='relu', padding='same'),
-            layers.Conv2DTranspose(16, kernel_size=15, strides=3, activation='relu', padding='same'),
-            layers.Conv2D(1, kernel_size=(3, 3), activation='sigmoid', padding='same')
-        ])
+    middle = layers.Conv2D(64, (2, 2), padding='same', activation='relu')(p3)
 
-    def call(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
-        return decoded
+    p4 = layers.Conv2DTranspose(32, kernel_size=(2, 2), strides=2, activation='relu', padding='same')(middle)
+    p5 = layers.Conv2DTranspose(16, kernel_size=(2, 2), strides=2, activation='relu', padding='same')(p4)
+    p6 = layers.Conv2DTranspose(8, kernel_size=(2, 2), strides=2, activation='relu', padding='same')(p5)
 
+    outputs = layers.Conv2D(1, kernel_size=(1, 1), padding='same')(p6)
 
-class ArtifactRemoverV2(keras.Model):
-    def __init__(self):
-        super().__init__()
+    model = tf.keras.Model(inputs, outputs)
 
-        self.model = keras.models.Sequential([
-            layers.Input(shape=(350, 175, 1)),
-            layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
-            layers.Conv2D(4, (3, 3), activation='relu', padding='same'),
-            layers.Conv2D(1, (2, 2), activation='relu', padding='same')
-        ])
-        print(self.model.summary())
-
-    def call(self, x):
-        return self.model(x)
+    return model
 
 
 def contracting_layers(x, n_filters, kernel_size, downsample_stride):
@@ -71,17 +52,17 @@ def expanding_layers(x, copied_features, n_filters, kernel_size, upsample_stride
 
 
 def artifact_remover_unet():
-    inputs = layers.Input(shape=(350, 175, 1))
+    inputs = layers.Input(shape=(344, 168, 1))
 
-    f1, p1 = contracting_layers(inputs, 16, 5, 5) 
-    f2, p2 = contracting_layers(p1, 32, 5, 5)
-    f3, p3 = contracting_layers(p2, 64, 7, 7)
+    f1, p1 = contracting_layers(inputs, 16, 2, 2) 
+    f2, p2 = contracting_layers(p1, 32, 2, 2)
+    f3, p3 = contracting_layers(p2, 64, 2, 2)
 
-    middle = layers.Conv2D(128, 5, padding='same', activation='relu')(p3)
+    middle = layers.Conv2D(128, 2, padding='same', activation='relu')(p3)
 
-    u6 = expanding_layers(middle, f3, 64, 7, 7)
-    u7 = expanding_layers(u6, f2, 32, 5, 5)
-    u8 = expanding_layers(u7, f1, 16, 5, 5)
+    u6 = expanding_layers(middle, f3, 64, 2, 2)
+    u7 = expanding_layers(u6, f2, 32, 2, 2)
+    u8 = expanding_layers(u7, f1, 16, 2, 2)
 
     outputs = layers.Conv2D(1, 1, padding='same')(u8)
     
@@ -106,6 +87,16 @@ def sobel_loss(target, predicted):
     sobel_predicted = tf.image.sobel_edges(predicted)
     
     return K.mean(K.square(sobel_target - sobel_predicted))
+
+
+def get_images(file_name):
+    im_indices = get_imaging_indices(25, 81, 512, 175, 350)
+    x_image = np.load(f"./images/data/{file_name}").reshape((350, 175))
+    y_image = np.load(f"./images/labels/{file_name}")[im_indices].reshape((350, 175))
+    x_image = tf.image.resize(preprocess_data(x_image)[tf.newaxis, ..., tf.newaxis], (344, 168))
+    y_image = tf.image.resize(preprocess_data(y_image)[tf.newaxis, ..., tf.newaxis], (344, 168))
+
+    return x_image, y_image
 
 
 def load_images(image_directory: str,
@@ -136,6 +127,11 @@ def load_images(image_directory: str,
     x_test_images = x_image_tensor[training_index:, :, :]
     y_test_images = y_image_tensor[training_index:, :, :]
 
+    x_train_images = tf.image.resize(x_train_images[..., tf.newaxis], (344, 168))
+    y_train_images = tf.image.resize(y_train_images[..., tf.newaxis], (344, 168))
+    x_test_images = tf.image.resize(x_test_images[..., tf.newaxis], (344, 168))
+    y_test_images = tf.image.resize(y_test_images[..., tf.newaxis], (344, 168))
+
     return x_train_images, y_train_images, x_test_images, y_test_images
 
 
@@ -151,39 +147,6 @@ def preprocess_data(image_array: np.array):
     return (image_array - np.min(image_array)) / np.ptp(image_array)
 
 
-def convolve_images(images):
-    convolved = images.copy()
-    psf = np.array([[0, 1, 2, 1, 0], [1, 2, 3, 2, 1], [2, 3, 4, 3, 2],
-                    [1, 2, 3, 2, 1], [0, 1, 2, 1, 0]], dtype='float64')
-    psf *= 1 / np.sum(psf)
-    n_images = convolved.shape[0]
-
-    for i in range(n_images):
-        convolved[i] = conv2(convolved[i], psf, 'same')
-
-    convolved = convolved[..., tf.newaxis]
-
-    return convolved
-
-
-def add_noise(images: np.array, mean_noise: float, std_dev_noise: float):
-    
-    noisy_images = images.copy()
-    n_images = noisy_images.shape[0]
-
-    for i in range(n_images):
-        gauss_noise = np.random.normal(loc=mean_noise,
-                                    scale=std_dev_noise,
-                                    size=noisy_images[i].size
-                                    )
-
-        gauss_noise = gauss_noise.reshape(*(noisy_images[i]).shape)
-        gauss_noise[gauss_noise < 0] = 0
-        noisy_images[i] = np.add(noisy_images[i], gauss_noise)
-
-    return noisy_images
-
-
 def plot_comparison(n_images, imaging_result, reconstructed_images, label_images):
 
     for i in range(n_images):
@@ -197,6 +160,7 @@ def plot_comparison(n_images, imaging_result, reconstructed_images, label_images
         plot_image(ax3, label_images[i], "Actual fracture image")
 
         plt.savefig(f"images/pngs/im{i}")
+        plt.show()
 
     print("Images saved.")
 
@@ -210,11 +174,11 @@ def plot_image(ax, image, title):
 
 def train_model(x_train, y_train):
     artifact_remover = artifact_remover_unet()
+    # artifact_remover = convolutional_autoencoder()
     # loss and optimizer
     optim = keras.optimizers.Adam(learning_rate=0.001)
-    metrics = ["accuracy"]
 
-    artifact_remover.compile(metrics=metrics, loss=sobel_loss, optimizer=optim)
+    artifact_remover.compile(loss=sobel_loss, optimizer=optim)
     artifact_remover.fit(x_train,
             y_train,
             epochs=200,
@@ -228,10 +192,6 @@ def train_model(x_train, y_train):
 
 if __name__ == "__main__":
     x_train, y_train, x_test, y_test = load_images("./images", 2050, 0.01)
-    x_train = x_train[..., tf.newaxis]
-    y_train = y_train[..., tf.newaxis]
-    x_test = x_test[..., tf.newaxis]
-    y_test = y_test[..., tf.newaxis]
     
     if str(sys.argv[1]) == "load":
         artifact_remover = tf.keras.models.load_model("./saved_model/trained_model.h5", compile=False)
@@ -245,9 +205,10 @@ if __name__ == "__main__":
     artifact_remover.evaluate(x_test, y_test)
 
     images_to_decode = 10
+    one_frac_x, one_frac_y = get_images("one_frac.npy")
 
     decoded_images = artifact_remover(x_test[:11])
 
     print("Average earth mover distance: ", calculate_emd(y_test[:images_to_decode+1,:,:,:], decoded_images))
 
-    plot_comparison(images_to_decode, x_test[:images_to_decode+1], decoded_images[:images_to_decode+1], y_test[:images_to_decode+1])
+    plot_comparison(images_to_decode, x_test[:11], decoded_images, y_test[:11])
