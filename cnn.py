452359fb587d9413d92b366b6ea00c4df30b3298
JohanMillberg <johanmillberg@me.com>
2022-12-14 22:40:08 +0100

    changed conv2dtranspose to upsampling->conv2d

--- cnn.py
+++ cnn.py
@@ -26,20 +26,23 @@ def residual_layer_block(inputs, n_filters, kernel_size, strides=1):
     return y
 
 
-def residual_network():
-    inputs = layers.Input(shape=(350, 175, 1))
+def residual_network(stride):
+    shape = (350, 175, 1) if stride == 5 else (344, 168, 1)
+    inputs = layers.Input(shape=shape)
 
-    x = layers.Conv2D(16, 5, padding='same', strides=5, activation='relu')(inputs)
+    x = layers.Conv2D(16, 5, padding='same', strides=stride, activation='relu')(inputs)
     x = layers.BatchNormalization()(x)
 
     for filters in (16, 32, 64):
         x = residual_layer_block(x, filters, 5)
 
-    x = layers.Conv2DTranspose(16, 5, 5, padding='same', activation='relu')(x)
+    x = layers.UpSampling2D(stride)(x)
+    x = layers.Conv2D(16, 5, padding='same', strides=stride, activation='relu')(inputs)
     outputs = layers.Conv2D(1, kernel_size=(1, 1), padding='same', activation='sigmoid')(x)
     model = tf.keras.Model(inputs, outputs)
 
     return model
+
 
 def convolutional_network():
     inputs = layers.Input(shape=(350, 175, 1))
@@ -60,16 +63,18 @@ def convolutional_network():
     return model
 
 
-def convolutional_autoencoder():
-    inputs = layers.Input(shape=(350, 175, 1))
+def convolutional_autoencoder(stride):
+    shape = (350, 175, 1) if stride == 5 else (344, 168, 1)
+    inputs = layers.Input(shape=shape) 
 
-    x = layers.Conv2D(16, (5, 5), activation='relu', padding='same', strides=5)(inputs)
+    x = layers.Conv2D(16, 5, stride, activation='relu', padding='same')(inputs)
 
-    x = layers.Conv2D(32, (5, 5), padding='same', activation='relu')(x)
-    x = layers.Conv2D(64, (5, 5), padding='same', activation='relu')(x)
-    x = layers.Conv2D(32, (5, 5), padding='same', activation='relu')(x)
+    x = layers.Conv2D(32, 5, padding='same', activation='relu')(x)
+    x = layers.Conv2D(64, 5, padding='same', activation='relu')(x)
+    x = layers.Conv2D(32, 5, padding='same', activation='relu')(x)
     
-    x = layers.Conv2DTranspose(16, kernel_size=(5, 5), strides=5, activation='relu', padding='same')(x)
+    x = layers.UpSampling2D(stride)(x)
+    x = layers.Conv2D(16, 5, activation='relu', padding='same')(x)
 
     outputs = layers.Conv2D(1, kernel_size=(1, 1), padding='same', activation='sigmoid')(x)
 
@@ -83,12 +88,14 @@ def contracting_layers(x, n_filters, kernel_size, downsample_stride):
     f = layers.BatchNormalization()(f)
 
     f = layers.Conv2D(n_filters, kernel_size, padding='same', activation='relu')(f)
-    p = layers.MaxPool2D(downsample_stride)(f)
+    p = layers.Conv2D(n_filters, kernel_size, strides=(downsample_stride, downsample_stride), padding='same', activation='relu')(f)
 
     return f, p
 
+
 def expanding_layers(x, copied_features, n_filters, kernel_size, upsample_stride):
-    x = layers.Conv2DTranspose(n_filters, kernel_size, upsample_stride, padding='same', activation='relu')(x)
+    x = layers.UpSampling2D(upsample_stride)(x)
+    x = layers.Conv2D(n_filters, kernel_size, padding='same', activation='relu')(x)
     x = layers.concatenate([x, copied_features])
     x = layers.Conv2D(n_filters, kernel_size, padding='same', activation='relu')(x)
     x = layers.Conv2D(n_filters, kernel_size, padding='same', activation='relu')(x)
@@ -96,10 +103,11 @@ def expanding_layers(x, copied_features, n_filters, kernel_size, upsample_stride
     return x
 
 
-def artifact_remover_unet():
-    inputs = layers.Input(shape=(350, 175, 1))
+def artifact_remover_unet(stride):
+    shape = (350, 175, 1) if stride == 5 else (344, 168, 1)
+    inputs = layers.Input(shape=shape)
 
-    f1, p1 = contracting_layers(inputs, 16, 5, 5) 
+    f1, p1 = contracting_layers(inputs, 16, 5, stride) 
 
     x = layers.Conv2D(32, 5, padding='same', activation='relu')(p1)
     x = layers.Conv2D(64, 5, padding='same', activation='relu')(x)
@@ -109,7 +117,7 @@ def artifact_remover_unet():
     x = layers.Conv2D(64, 5, padding='same', activation='relu')(middle)
     x = layers.Conv2D(32, 5, padding='same', activation='relu')(x)
 
-    u8 = expanding_layers(x, f1, 16, 5, 5)
+    u8 = expanding_layers(x, f1, 16, 5, stride)
 
     outputs = layers.Conv2D(1, 1, padding='same', activation='sigmoid')(u8)
     
@@ -234,9 +242,10 @@ def plot_comparison(n_images,
                     label_images,
                     model_name,
                     loss_name,
+                    stride,
                     start_index):
     
-    save_path = f"images/pngs/{model_name}_{loss_name}"
+    save_path = f"images/pngs/{model_name}_{loss_name}_{stride}"
 
     if not os.path.exists(save_path):
         os.makedirs(save_path)
@@ -263,15 +272,15 @@ def plot_image(ax, image, title):
     ax.get_yaxis().set_visible(False) 
 
 
-def train_model(x_train, y_train, model_name, loss_name):
+def train_model(x_train, y_train, model_name, loss_name, stride):
     if model_name == "UNet":
-        artifact_remover = artifact_remover_unet()
+        artifact_remover = artifact_remover_unet(stride)
     elif model_name == "ConvNN":
         artifact_remover = convolutional_network()
     elif model_name == "ResNet":
-        artifact_remover = residual_network()
+        artifact_remover = residual_network(stride)
     elif model_name == "ConvAuto":
-        artifact_remover = convolutional_autoencoder()
+        artifact_remover = convolutional_autoencoder(stride)
     else:
         raise NotImplementedError()
 
@@ -282,13 +291,13 @@ def train_model(x_train, y_train, model_name, loss_name):
     artifact_remover.compile(loss=loss, optimizer=optim)
     artifact_remover.fit(x_train,
             y_train,
-            epochs=200,
+            epochs=200 if stride == 5 else 10,
             shuffle=False,
-            batch_size=10,
+            batch_size=10 if stride == 5 else 5,
             verbose=2)
             #validation_data=(x_test, y_test))
 
-    artifact_remover.save(f"./saved_model/{model_name}_{loss_name}_trained_model.h5")
+    artifact_remover.save(f"./saved_model/{model_name}_{loss_name}_{stride}_trained_model.h5")
     return artifact_remover
 
 if __name__ == "__main__":
@@ -301,8 +310,10 @@ if __name__ == "__main__":
     if not os.path.exists("./saved_model/"):
         os.makedirs("./saved_model/")
 
+    stride = int(sys.argv[3])
+
     resize = False
-    if (sys.argv[4] == "True"):
+    if (stride == 2):
         resize = True
 
     model_name = sys.argv[1]
@@ -310,8 +321,8 @@ if __name__ == "__main__":
 
     x_train, y_train, x_test, y_test = load_images("./images", 2500, 0.05, resize)
     
-    if str(sys.argv[3]) == "load":
-        artifact_remover = tf.keras.models.load_model(f"./saved_model/{model_name}_{loss_name}_trained_model.h5", compile=False)
+    if str(sys.argv[4]) == "load":
+        artifact_remover = tf.keras.models.load_model(f"./saved_model/{model_name}_{loss_name}_{stride}_trained_model.h5", compile=False)
 
         # set loss and optimizer here
         optim = keras.optimizers.Adam(learning_rate=0.001)
@@ -320,9 +331,10 @@ if __name__ == "__main__":
         artifact_remover.compile(metrics=metrics, loss=loss, optimizer=optim)
 
     else:
-        artifact_remover = train_model(x_train, y_train, model_name, loss_name)
+        print(f"\nTraining model {model_name} with loss {loss_name}...\n")
+        artifact_remover = train_model(x_train, y_train, model_name, loss_name, stride)
 
-    artifact_remover.evaluate(x_test, y_test)
+    #artifact_remover.evaluate(x_test, y_test)
 
     one_frac_x, one_frac_y = get_images("one_frac.npy", resize)
     point_scatter_x, point_scatter_y = get_images("point_scatter.npy", resize)
@@ -351,5 +363,5 @@ if __name__ == "__main__":
     print("Average mean squared error: ", np.mean(mses))
     print("Average SSIM: ", np.mean(ssims))
 
-    plot_comparison(4, x_special, special_images, y_special, model_name, loss_name, 0)
-    plot_comparison(im_per_eval, x_test[:im_per_eval+1], decoded_images, y_test[:im_per_eval+1], model_name, loss_name, 4)
+    plot_comparison(4, x_special, special_images, y_special, model_name, loss_name, stride, 0)
+    plot_comparison(im_per_eval, x_test[:im_per_eval+1], decoded_images, y_test[:im_per_eval+1], model_name, loss_name, stride, 4)
